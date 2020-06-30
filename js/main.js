a0f48b333f8313eb0452bcf096182274effbce38
Chris Armendarez <chris.michael.armo@gmail.com>
2020-06-30 16:57:04 -0700

    Add capitalize & title functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("Hello, world!");
+const greeting = 'hello, world!';
+
+function capitalize(word) {
+  return `${word[0].toUpperCase()}${word.substring(1)}`;
+}
+
+function title(words) {
+  return words.split(' ').map(capitalize).join(' ');
+}
+
+console.log(title(greeting));

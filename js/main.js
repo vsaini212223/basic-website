d7916e035ab45589f7d4acc523739b4f6a53903a
Chris Armendarez <chris.michael.armo@gmail.com>
2020-06-30 16:35:55 -0700

    Add greet method

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("Hello, world!");
+function greet(greeting = "Hello", name = 'world', excited = true) {
+  return `${greeting}, ${name}${excited ? '!' : '.'}`;
+}
+
+greet('Sup', 'Marilyn', false);
+greet('Hey there', 'Pistachio');
